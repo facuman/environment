bb333145a4a7fda071875d2654a2e7776d0655f1
Facundo de Guzmán <facudeguzman@gmail.com>
2009-08-07 00:57:30 -0300

    Virtual environments for python 2.5 and 2.6 ready.
    On them: ipython, rlcompleter2, pyreadline

---
+++ python/2.5/lib/python2.5/os.py
@@ -0,0 +1,1 @@
+/home/fdeguzman/environment/binaries/ActivePython-2.5.4.4-linux-x86_64/lib/python2.5/os.py
---
+++ python/2.6/lib/python2.6/sre_compile.py
@@ -0,0 +1,1 @@
+/home/fdeguzman/environment/binaries/ActivePython-2.6.2.2-linux-x86_64/lib/python2.6/sre_compile.py
