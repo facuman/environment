ffe0e9b79954f99fd1809b1bebca72a21e4ad13f
Facundo de Guzmán <facudeguzman@gmail.com>
2010-01-05 23:31:02 -0300

    Being using org mode daily. Just little customizations to it

--- emacs/modes/custom/facuman-org.el
+++ emacs/modes/custom/facuman-org.el
@@ -15,9 +15,35 @@
 (setq org-clock-persist 'history)
 (org-clock-persistence-insinuate)
 
+;; (add-to-list 'iimage-mode-image-regex-alist
+;;              (cons (concat "\\[\\[file:\\(~?" iimage-mode-image-filename-regex
+;;                            "\\)\\]")  1))
+
+;; (defun org-toggle-iimage-in-org ()
+;;   "display images in your org file"
+;;   (interactive)
+;;   (if (face-underline-p 'org-link)
+;;       (set-face-underline-p 'org-link nil)
+;;     (set-face-underline-p 'org-link t))
+;;   (iimage-mode))
+
 
 (defun my-org-startup ()
   "Org mode default settings."
+  (local-set-key "\M-n" 'outline-next-visible-heading)
+  (local-set-key "\M-p" 'outline-previous-visible-heading)
+  ;; table
+  (local-set-key "\M-\C-w" 'org-table-copy-region)
+  (local-set-key "\M-\C-y" 'org-table-paste-rectangle)
+  (local-set-key "\M-\C-l" 'org-table-sort-lines)
+  ;; display images
+;;  (local-set-key "\M-I" 'org-toggle-iimage-in-org)
+  ;; fix tab
+  (local-set-key "\C-y" 'yank)
+  ;; yasnippet (allow yasnippet to do it's thing in org files)
+  ;; (make-variable-buffer-local 'yas/trigger-key)
+  ;; (setq yas/trigger-key [tab])
+  ;; (define-key yas/keymap [tab] 'yas/next-field-group)
   )
 
 (add-hook 'org-mode-hook 'my-org-startup)
